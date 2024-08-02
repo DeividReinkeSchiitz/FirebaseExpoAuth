2825baf0fa1ffef38a3616638431452a3a931eaa
Deivd Reinke Schiitz <55961826+DeividReinkeSchiitz@users.noreply.github.com>
2024-08-02 14:53:35 -0400

    Update GoogleConfig.js

--- src/config/GoogleConfig.js
+++ src/config/GoogleConfig.js
@@ -1,8 +1,8 @@
 const GoogleConfig = {
   androidClientId:
-    "260499478397-3cikf4brik5iq0rpaa8v89pof535j18i.apps.googleusercontent.com",
+    "androidid.apps.googleusercontent.com",
   iosClientId:
-    "260499478397-prg2akkmf5tvf7cmgbfmin4b9738hgvu.apps.googleusercontent.com",
+    "ios.apps.googleusercontent.com",
   scopes: ["profile", "email"]
 };
 
